ca94dc5a3a1d72d85b4d5cc92cfbb2a9c32762e6
digital.intern2 <digital.intern2@hayleysadvantis.com>
2026-05-20 15:50:39 +0530

    Remove Insights Screen

--- FinanceFlow/app/src/main/java/com/example/financeflow/ui/insights/InsightsScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.financeflow.ui.insights
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun InsightsScreen() {
-    Text("Insights Screen")
-}
