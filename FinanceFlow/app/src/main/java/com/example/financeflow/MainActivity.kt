b4c2ddc44fcebd150262e1c5c35dfea060f14797
IT23231528chamudi <it23231528@my.sliit.lk>
2026-05-22 20:52:16 +0530

    Refactor MainActivity to implement theme toggle functionality

--- FinanceFlow/app/src/main/java/com/example/financeflow/MainActivity.kt
+++ FinanceFlow/app/src/main/java/com/example/financeflow/MainActivity.kt
@@ -4,42 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import com.example.financeflow.navigation.AppNavGraph
 import com.example.financeflow.ui.theme.FinanceFlowTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var isDarkTheme by rememberSaveable { mutableStateOf(false) }
 
-            FinanceFlowTheme {
-
-                AppNavGraph()
+            FinanceFlowTheme(darkTheme = isDarkTheme) {
+                AppNavGraph(
+                    isDarkTheme = isDarkTheme,
+                    onThemeToggle = { isDarkTheme = !isDarkTheme }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FinanceFlowTheme {
-        Greeting("Android")
     }
 }
